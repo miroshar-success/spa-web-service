d931c1ef399c71366933e59bce92b3df90515738
oskarmast <oskarmast9436@gmail.com>
2018-05-08 14:39:25 +0300

    node js api sample

---
+++ api/index.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const userRoutes = require('./server/user/user.route');
+
+const router = express.Router(); // eslint-disable-line new-cap
+
+// TODO: use glob to match *.route files
+
+/** GET /health-check - Check service health */
+router.get('/health-check', (req, res) =>
+  res.send('OK')
+);
+
+// mount user routes at /users
+router.use('/users', userRoutes);
+
+module.exports = router;
