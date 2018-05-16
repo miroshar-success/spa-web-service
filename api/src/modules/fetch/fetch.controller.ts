99953881b910b4930206989c63170b8bcee810eb
oskarmast <oskarmast9436@gmail.com>
2018-05-16 18:17:15 +0300

    mq

--- api/src/modules/fetch/fetch.controller.ts
+++ api/src/modules/fetch/fetch.controller.ts
@@ -3,10 +3,13 @@ import {Body, Controller, Post} from '@nestjs/common';
 import {FetchExploreDto, FetchDto} from './fetch.dto';
 import {FetchService} from "./fetch.service";
 
+import {MqApiServer, Controller1, Controller2} from "./mq";
+
 @Controller('fetch')
 export class FetchController {
 
     constructor(private readonly fetchService: FetchService) {
+        new MqApiServer({basePath:'api',clients:['telegram','viber'],connection:{}}, [Controller1,Controller2]);
     }
 
     @Post('/explore')
