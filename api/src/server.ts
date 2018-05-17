d507294cc5ff75026be01765f9b39ef6116900df
oskarmast <oskarmast9436@gmail.com>
2018-05-17 10:34:38 +0300

    comment example of use

--- api/src/server.ts
+++ api/src/server.ts
@@ -8,14 +8,14 @@ import {ScannerService} from './modules/scanner/scanner.service';
 
 async function bootstrap() {
 
-    const scanner: ScannerService = new ScannerService();
+  /*  const scanner: ScannerService = new ScannerService();
     const url = 'https://news.tut.by/world/';
     const allExamples = await scanner.fetchAll(url);
     const promises = allExamples.sample.map(async example =>  {
         return await scanner.fetchOne(url, example.selector);
     });
     const allDetails = await Promise.all(promises);
-    const app = await NestFactory.create(ApplicationModule);
+    const app = await NestFactory.create(ApplicationModule);*/
 
     // VALIDATION CONFIG
     app.use(bodyParser.json());
