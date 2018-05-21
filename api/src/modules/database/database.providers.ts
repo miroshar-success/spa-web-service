955f8e6a9abdf667b4bbdb45040bfa7a690b2d30
oskarmast <oskarmast9436@gmail.com>
2018-05-21 15:26:32 +0300

    Add database helpers for insert/remove data (for persons and fetchs)

--- api/src/modules/database/database.providers.ts
+++ api/src/modules/database/database.providers.ts
@@ -1,11 +1,17 @@
 import * as mongoose from 'mongoose';
+import Person from '../person/person.schema';
+import { generatePersons, generateFetchs, removeData } from './database.helpers';
 
 export const databaseProviders = [
   {
     provide: 'DbConnectionToken',
     useFactory: async (): Promise<mongoose.Connection> => {
       (mongoose as any).Promise = global.Promise;
-      return await mongoose.connect('mongodb://beagle-mongo:27017/beagle');
+      return await mongoose.connect('mongodb://beagle-mongo:27017/beagle', () => {
+        // removeData();
+        // generatePersons(25);
+        // generateFetchs(25);
+      });
     }
   }
 ];
