6cc780f31283c1a8da7edda392b480237dffe9d2
oskarmast <oskarmast9436@gmail.com>
2018-05-22 12:17:13 +0300

    UI - move

---
+++ beagle-web/api/src/clients/app.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://beagle-mongo:27017/beagle-web')]
+})
+export class ApplicationModule { }
