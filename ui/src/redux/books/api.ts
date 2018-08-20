a261629abde8d91a2945238c4942afe3e305ca6e
oskarmast <oskarmast9436@gmail.com>
2018-08-20 16:35:21 +0300

    dsf

--- ui/src/redux/books/api.ts
+++ ui/src/redux/books/api.ts
@@ -14,6 +14,7 @@ export const searchBooks = (search: string) => {
   return axios.get(`/data/books/find?search=${search}`);
 }
 
+<<<<<<< HEAD
 export const addBooks = (name: string, author: string, cost: number, genre: string,) => {    
   return axios.post(`data/books/newbook?genre=${genre}&cost=${cost}&author=${author}&name=${name}`)
   .then(response => {
